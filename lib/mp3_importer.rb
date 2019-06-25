d676793e4a8caea634099d18f2754426a2a83d80
nbaugh1@gmail.com <nbaugh1@gmail.com>
2019-06-25 19:55:47 -0400

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+class MP3Importer
+    attr_accessor :path
+
+    def initialize(path)
+        self.path = path
+    end
+
+    def files
+        array = Dir.glob(path + "/*.mp3")
+        array.map { |file| file.gsub(path + "/", "") }
+    end
+
+    def import
+        files.each do |file|
+            Song.new_by_filename(file)
+        end
+    end
+end
