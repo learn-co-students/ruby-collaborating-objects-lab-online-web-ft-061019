acde84f5a97a84af646b0054fe45590f29dfd53c
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-07 15:13:31 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,8 @@
+require 'pry'
 class Artist
   attr_accessor :name, :songs
   @@all = []  #stores all instances of Artist 
-
+  @@songs = @songs
   def initialize(name)
     @name = name
     @songs = []
@@ -42,7 +43,8 @@ class Artist
 
   def print_songs #instance method iterates through @songs of an instance of Artist
     self.songs.each do |song|
-      song.length 
+      @songs.length 
+      puts song.name
     end 
   end
 end
